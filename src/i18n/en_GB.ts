339a69fcc2d31f5b828ed83286ef328426710bdd
Pavel Nikitaev <devoll@devoll.ru>
2021-05-02 19:01:58 +0300

    extend language strings

--- src/i18n/en_GB.ts
+++ src/i18n/en_GB.ts
@@ -2,6 +2,7 @@ namespace Language {
     export const en_GB = {
         "CHOOSE_SRC_FOLDER": "Choose folder with JPEGs which need to trace",
         "CHOOSE_SRC_FOLDER_ERROR": "Error folder choose",
-        "CHOOSE_SRC_FOLDER_EMPTY": "The folder has no JPEGs"
+        "CHOOSE_SRC_FOLDER_EMPTY": "The folder has no JPEGs",
+        "PROCESSED_FILED": "Files processed"
     }
 }
--- src/i18n/ru_RU.ts
+++ src/i18n/ru_RU.ts
@@ -2,6 +2,7 @@ namespace Language {
   export const ru_RU = {
     "CHOOSE_SRC_FOLDER": "Выберите каталог с файлами JPG, которые необходимо трасировать",
     "CHOOSE_SRC_FOLDER_ERROR": "Ошибка выбора каталога",
-    "CHOOSE_SRC_FOLDER_EMPTY": "В каталоге отсутствуют файлы JPG"
+    "CHOOSE_SRC_FOLDER_EMPTY": "В каталоге отсутствуют файлы JPG",
+    "PROCESSED_FILED": "Файлов обработано"
   }
 }
